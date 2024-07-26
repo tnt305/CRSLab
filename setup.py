adc66cc0cf242c56de29ccc735e7741f94ca67ea
Vu Le <levu040102@gmail.com>
2024-07-26 03:48:07 +0000

    Replace this generic exception class with a more specific one.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 try:
     import torch
     import torch_geometric
-except Exception:
-    raise Exception('Please install PyTorch and PyTorch Geometric manually first.\n' +
-                    'View CRSLab GitHub page for more information: https://github.com/RUCAIBox/CRSLab')
+except ImportError:
+    raise ImportError('Please install PyTorch and PyTorch Geometric manually first.\n'
+                      'View CRSLab GitHub page for more information: https://github.com/RUCAIBox/CRSLab')
 
 setup_requires = []
 
